efee3f7eb1134d8c4b0af8ca569554e3833d5ff5
wizowash-collab <wizowash@gmail.com>
2025-08-29 13:06:02 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -165,7 +165,7 @@ window.castVote = function(title, points) {
 window.checkPassword = function() {
   const input = document.getElementById("passwordInput").value;
   const error = document.getElementById("loginError");
-  if (input === "f@m!lY") {
+  if (input === "1234") {
     document.getElementById("loginScreen").style.display = "none";
     document.getElementById("appContent").style.display = "block";
     initializeApp();
